979cd880e0a1da7cd4797c6083629136c94b7dba
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-07-16 18:32:27 -0500

    chore(docker): update `iota` image tag

--- docker/iota/build.sh
+++ docker/iota/build.sh
@@ -3,4 +3,4 @@
 # Modifications Copyright (c) 2024 IOTA Stiftung
 # SPDX-License-Identifier: Apache-2.0
 export CARGO_BUILD_FEATURES=indexer
-./../utils/build-script.sh --image-tag "angelovtt/iota:1.4.0-alpha"
+./../utils/build-script.sh --image-tag "angelovtt/iota:v1.3.1-rc"
